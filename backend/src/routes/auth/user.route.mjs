d21aa6aaa5d5aa05d4f6baa779584465c8d44df5
claudiobentodaladev <programmer.claudio@gmail.com>
2025-12-28 14:35:29 +0100

    allowed to create user

--- backend/src/routes/auth/user.route.mjs
+++ backend/src/routes/auth/user.route.mjs
@@ -1,26 +1,24 @@
 import { Router } from "express";
+import mysql from "../../database/mysql/db.connection.mjs";
+import { hashPassword } from "../../utils/hashPassword.mjs";
 
 const router = Router()
 
 router.post("/sign", (request, response) => {
-    const [{ email, password, role }, profile] = request.body;
+    const [{ email, password, role },profile] = request.body.user;
 
-    switch (role) {
-        case "admin":
-            const { name_, photo_url_ } = profile;
-            break;
-        case "eleitor":
-            const { name, surname, sex, born_date, photo_url } = profile;
-            break;
-        default:
-            return response.status(400).json({ message: "role is invalid!", advice: "must be admin or eleitor" })
-            break;
+    if (!["admin", "eleitor"].includes(role)) {
+        return response.status(400).json({ message: "role is invalid!", advice: "must be admin or eleitor" })
     }
     //insert user
+    mysql.execute("insert into users values(default,?,?,?,default)", [email, hashPassword(password), role], (err, result) => {
+        if (err) {
+            return response.status(400).json(err)
+        }
+        return response.status(201).json(result) // object with id: insertId
+    })
     // find user_id by email in database
     // use the found user_id to insert into profile
-    return response.status(201).send("created")
-
 })
 
 export default router;
