5fd561f3c982753bf388190c837f00a04706339f
claudiobentodaladev <programmer.claudio@gmail.com>
2025-12-30 10:56:53 +0100

    created the edit user's profile endpoint

--- backend/src/routes/user/profile.user.mjs
+++ backend/src/routes/user/profile.user.mjs
@@ -5,43 +5,78 @@ import { Profile } from "../../database/mongodb/schema/user.schema.mjs";
 const router = Router()
 
 router.get("/", authenticated, async (request, response) => {
-    const { id, email, role } = request.user;
-    const user = { email: email, role: role }
-    const { name, surname, sex, born_date, photo_url } = await Profile.findOne({ user_id: id })
+    try {
+        const { id, email, role } = request.user;
+        const user = { email: email, role: role }
+        const { name, surname, sex, born_date, photo_url } = await Profile.findOne({ user_id: id })
 
-    switch (role) {
-        case "admin":
-            return response.status(200).json({
-                user: user,
-                profile: {
-                    name: name,
-                    photo_url: photo_url
-                }
-            })
-            break;
+        switch (role) {
+            case "admin":
+                return response.status(200).json({
+                    user: user,
+                    profile: {
+                        name: name,
+                        photo_url: photo_url
+                    }
+                })
+                break;
 
-        case "eleitor":
-            return response.status(200).json({
-                user: user,
-                profile: {
-                    name: name,
-                    surname: surname,
-                    sex: sex,
-                    born_date: born_date,
-                    photo_url: photo_url
-                }
-            })
-            break;
+            case "eleitor":
+                return response.status(200).json({
+                    user: user,
+                    profile: {
+                        name: name,
+                        surname: surname,
+                        sex: sex,
+                        born_date: born_date,
+                        photo_url: photo_url
+                    }
+                })
+                break;
 
-        default:
-            return response.sendStatus(400)
-            break;
+            default:
+                return response.sendStatus(400)
+                break;
+        }
+    } catch (err) {
+        return response
     }
 
 });
 
-router.patch("/", authenticated, async (request, response) => {})
+router.patch("/", authenticated, async (request, response) => {
+    try {
+        const { id } = request.user
 
-router.patch("/", authenticated, async (request, response) => {})
+        const allowedFields = ["name", "surname", "photo_url"];
+
+        const updateData = {};
+
+        for (const field of allowedFields) {
+            if (request.body[field] !== undefined) {
+                updateData[field] = request.body[field];
+            }
+        }
+
+        if (Object.keys(updateData).length === 0) {
+            return res.status(400).json({
+                error: "no valid data to update!"
+            });
+        }
+
+        const profile = await Profile.updateOne({ user_id: id }, { $set: updateData }, { new: true })
+
+        if (!profile) {
+            return response.status(404).json({
+                error: "user not found!"
+            });
+        }
+
+        return response.status(201).json(profile)
+
+    } catch (err) {
+        return response.status(500).json(err);
+    }
+})
 
 export default router;
