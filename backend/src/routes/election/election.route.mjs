99e245d9e5a87b2f2353da8cb90a273e01716819
claudiobentodaladev <programmer.claudio@gmail.com>
2026-01-01 14:54:27 +0100

    created endpoint to get elections from an admin

--- backend/src/routes/election/election.route.mjs
+++ backend/src/routes/election/election.route.mjs
@@ -2,9 +2,11 @@ import { Router } from "express";
 import { isAdmin, validator } from "../../utils/middlewares.mjs";
 import { createElection } from "../../validator/validator.mjs";
 import create from "./create.election.mjs";
+import get from "./get.election.mjs";
 
 const router = Router()
 
 router.use("/", isAdmin, createElection, validator, create)
+router.use("/", isAdmin, get)
 
 export default router;
---
+++ backend/src/routes/election/get.election.mjs
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import mysql from "../../database/mysql/db.connection.mjs";
+
+const router = Router()
+
+router.get("/", (request, response) => {
+    const { user } = request;
+    mysql.execute("select * from elections where user_id = ?", [user.id], (err, result) => {
+        if (err) return response.status(500).json(err)
+        return response.status(200).json(result)
+    })
+})
+
+export default router;
