7ac0c062fe32e3ae764a686840e50d83c2801cfe
claudiobentodaladev <programmer.claudio@gmail.com>
2026-01-02 10:23:23 +0100

    added the main value to use in endpoint

--- backend/src/routes/election/edit.election.mjs
+++ backend/src/routes/election/edit.election.mjs
@@ -1,9 +1,12 @@
 import { Router } from "express";
+import mysql from "../../database/mysql/db.connection.mjs";
 
 const router = Router();
 
 router.patch("/:election_id", (request, response) => {
     // Dinamic edit data, with no required field, like edit(patch) profile
+    const { user } = request;
+    const { election_id } = request.params;
     return response.status(200).send("election!")
 })
 
