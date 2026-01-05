5378308d58298d60b7632ba6e6150fcd6f76b045
claudiobentodaladev <programmer.claudio@gmail.com>
2026-01-05 23:03:47 +0100

    a new dump

--- database/dump/electionSys.sql
+++ database/dump/electionSys.sql
@@ -1,10 +1,10 @@
-CREATE DATABASE  IF NOT EXISTS `electionSys` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE  IF NOT EXISTS `electionSys` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `electionSys`;
 -- MySQL dump 10.13  Distrib 8.0.36, for Linux (x86_64)
 --
 -- Host: localhost    Database: electionSys
 -- ------------------------------------------------------
--- Server version	8.0.43-0ubuntu0.24.04.2
+-- Server version	8.0.43
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -38,17 +38,8 @@ CREATE TABLE `audit_logs` (
   CONSTRAINT `audit_logs_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
   CONSTRAINT `audit_logs_ibfk_2` FOREIGN KEY (`election_id`) REFERENCES `elections` (`id`),
   CONSTRAINT `audit_logs_ibfk_3` FOREIGN KEY (`candidate_id`) REFERENCES `candidates` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `audit_logs`
---
-
-LOCK TABLES `audit_logs` WRITE;
-/*!40000 ALTER TABLE `audit_logs` DISABLE KEYS */;
-/*!40000 ALTER TABLE `audit_logs` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `candidates`
@@ -60,25 +51,16 @@ DROP TABLE IF EXISTS `candidates`;
 CREATE TABLE `candidates` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `participation_id` bigint NOT NULL,
-  `photo_group` text,
+  `logo_group_url` text,
   `group_name` varchar(35) NOT NULL,
   `description` text,
-  `status` tinyint(1) DEFAULT '0',
-  `created_at` datetime DEFAULT (curdate()),
+  `status` enum('eligible','ineligible','blocked') NOT NULL DEFAULT 'ineligible',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   KEY `participation_id` (`participation_id`),
   CONSTRAINT `candidates_ibfk_3` FOREIGN KEY (`participation_id`) REFERENCES `participation` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `candidates`
---
-
-LOCK TABLES `candidates` WRITE;
-/*!40000 ALTER TABLE `candidates` DISABLE KEYS */;
-/*!40000 ALTER TABLE `candidates` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `candidates_propose`
@@ -99,15 +81,6 @@ CREATE TABLE `candidates_propose` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `candidates_propose`
---
-
-LOCK TABLES `candidates_propose` WRITE;
-/*!40000 ALTER TABLE `candidates_propose` DISABLE KEYS */;
-/*!40000 ALTER TABLE `candidates_propose` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `elections`
 --
 
@@ -117,26 +90,17 @@ DROP TABLE IF EXISTS `elections`;
 CREATE TABLE `elections` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `user_id` bigint NOT NULL,
-  `title` varchar(80) NOT NULL,
-  `description` text,
+  `title` varchar(80) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` text COLLATE utf8mb4_unicode_ci,
   `start_at` datetime NOT NULL,
   `end_at` datetime NOT NULL,
-  `status` enum('active','ongoing','closed') DEFAULT NULL,
+  `status` enum('active','ongoing','closed') COLLATE utf8mb4_unicode_ci DEFAULT 'active',
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`),
   CONSTRAINT `elections_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
   CONSTRAINT `check_date` CHECK ((`start_at` < `end_at`))
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `elections`
---
-
-LOCK TABLES `elections` WRITE;
-/*!40000 ALTER TABLE `elections` DISABLE KEYS */;
-/*!40000 ALTER TABLE `elections` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `participation`
@@ -149,23 +113,15 @@ CREATE TABLE `participation` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `user_id` bigint NOT NULL,
   `election_id` bigint NOT NULL,
-  `status` tinyint(1) DEFAULT '0',
+  `status` enum('eligible','ineligible','voted') NOT NULL DEFAULT 'ineligible',
   PRIMARY KEY (`id`),
+  UNIQUE KEY `participateOne` (`user_id`,`election_id`),
   KEY `user_id` (`user_id`),
   KEY `election_id` (`election_id`),
   CONSTRAINT `participation_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
   CONSTRAINT `participation_ibfk_2` FOREIGN KEY (`election_id`) REFERENCES `elections` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `participation`
---
-
-LOCK TABLES `participation` WRITE;
-/*!40000 ALTER TABLE `participation` DISABLE KEYS */;
-/*!40000 ALTER TABLE `participation` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `propose_comentary`
@@ -189,15 +145,6 @@ CREATE TABLE `propose_comentary` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `propose_comentary`
---
-
-LOCK TABLES `propose_comentary` WRITE;
-/*!40000 ALTER TABLE `propose_comentary` DISABLE KEYS */;
-/*!40000 ALTER TABLE `propose_comentary` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `users`
 --
 
@@ -212,18 +159,8 @@ CREATE TABLE `users` (
   `created_at` date DEFAULT (curdate()),
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `users`
---
-
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'claudio@enedi.com','$2b$10$W0IQsUDOuCoeW5xhq7sJsORy5Im7pXAlSyCVeV/qOmhPSt3FJqkRy','admin','2025-12-25'),(3,'euclides@enedi.com','$2b$10$cvgTlhmlB2CCXVj4mYbssemrH7KR8fO32rspObHRrFEpZEgWGaB5q','eleitor','2025-12-25'),(4,'general@enedi.com','$2b$10$zbv3Y3lv5NWtq0CoHuK6jOqYr.Hezo.XOilny0YaS7DswPD8Qc26m','admin','2025-12-25');
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `vote`
@@ -238,6 +175,7 @@ CREATE TABLE `vote` (
   `candidate_id` bigint NOT NULL,
   `created_at` datetime DEFAULT (curdate()),
   PRIMARY KEY (`id`),
+  UNIQUE KEY `voteOne` (`participation_id`,`candidate_id`),
   KEY `candidate_id` (`candidate_id`),
   KEY `participation_id` (`participation_id`),
   CONSTRAINT `vote_ibfk_2` FOREIGN KEY (`candidate_id`) REFERENCES `candidates` (`id`),
@@ -246,13 +184,12 @@ CREATE TABLE `vote` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `vote`
+-- Dumping events for database 'electionSys'
 --
 
-LOCK TABLES `vote` WRITE;
-/*!40000 ALTER TABLE `vote` DISABLE KEYS */;
-/*!40000 ALTER TABLE `vote` ENABLE KEYS */;
-UNLOCK TABLES;
+--
+-- Dumping routines for database 'electionSys'
+--
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -263,4 +200,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-12-27 15:49:17
+-- Dump completed on 2026-01-05 23:02:57
