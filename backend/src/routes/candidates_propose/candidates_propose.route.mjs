2f02ecc0612653b0fc62b29b3b5b877bc14c8cf2
claudiobentodaladev <programmer.claudio@gmail.com>
2026-01-06 19:20:13 +0100

    created edit cadidate propose file

--- backend/src/routes/candidates_propose/candidates_propose.route.mjs
+++ backend/src/routes/candidates_propose/candidates_propose.route.mjs
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import create from "./create.candidates_propose.mjs";
 import get from "./get.candidates_propose.mjs";
+import edit from "./edit.candidates_propose.mjs";
 
 const router = Router()
 
 router.use("/", create)
 router.use("/", get)
+router.use("/", edit)
 
 export default router;
---
+++ backend/src/routes/candidates_propose/edit.candidates_propose.mjs
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import mysql from "../../database/mysql/db.connection.mjs";
+
+const router = Router();
+
+router.patch("/candidates_propose_id", (request, response) => {
+
+});
+
+export default router;
