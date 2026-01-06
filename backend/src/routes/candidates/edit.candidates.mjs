5a0c1dd225e201ee54ce80083c947bca2bec5306
claudiobentodaladev <programmer.claudio@gmail.com>
2026-01-06 12:16:46 +0100

    created endpoint to edit candidates

---
+++ backend/src/routes/candidates/edit.candidates.mjs
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import mysql from "../../database/mysql/db.connection.mjs";
+
+const router = Router();
+
+router.patch("/:election_id", (request, response) => {
+    const { election_id } = request.params;
+});
+
+export default router;
--- backend/src/routes/candidates/get.candidates.route.mjs
+++ backend/src/routes/candidates/get.candidates.route.mjs
@@ -29,17 +29,29 @@ router.get("/:election_id", isAdmin, (request, response) => {
     const { user } = request;
     const { election_id } = request.params;
 
-    mysql.execute("SELECT id FROM participation WHERE election_id = ?", [election_id], (err, result) => {
-        if (err) return response.status(500).json(err)
-        if (result.length === 0) return response.status(404).json({ found: false, message: "election not found!" })
-
-        const participations_ids = joinedArray(result)
-
-        mysql.execute(`SELECT * FROM candidates WHERE participation_id IN(?)`, [participations_ids], (err, result) => {
+    mysql.execute(
+        "SELECT id FROM elections WHERE id = ? AND user_id = ?",
+        [election_id, user.id], (err, result) => {
             if (err) return response.status(500).json(err)
-            return response.status(200).json(result)
-        })
-    })
+            if (result.length === 0) return response.status(404).json({ found: false, message: "election not found!" })
+
+            const [{ id }] = result;
+
+            mysql.execute("SELECT id FROM participation WHERE election_id = ?", [id], (err, result) => {
+                if (err) return response.status(500).json(err)
+                if (result.length === 0) return response.status(404).json({ found: false, message: "election not found!" })
+
+                const participations_ids = joinedArray(result)
+
+                mysql.execute(`SELECT * FROM candidates WHERE participation_id IN(?)`, [participations_ids], (err, result) => {
+                    if (err) return response.status(500).json(err)
+                    return response.status(200).json(result)
+                })
+            })
+
+        }
+    )
+
 
 })
 
