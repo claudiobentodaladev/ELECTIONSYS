25e83ed477d9bf474d7b75947ed2a1e143884d88
claudiobentodaladev <programmer.claudio@gmail.com>
2026-01-07 11:12:18 +0100

    created file to get edit comentary endpoint

---
+++ backend/src/routes/candidates/propose/comentary/edit.propose_comentary.mjs
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import mysql from "../../../../database/mysql/db.connection.mjs";
+
+const router = Router();
+
+router.patch("/", (request, response) => {
+
+})
+
+export default router;
--- backend/src/routes/candidates/propose/comentary/propose_comentary.route.mjs
+++ backend/src/routes/candidates/propose/comentary/propose_comentary.route.mjs
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import create from "./create.propose_comentary.mjs";
 import get from "./get.propose_comentary.mjs";
+import edit from "./edit.propose_comentary.mjs";
 
 const router = Router();
 
 router.use("/", create)
 router.use("/", get)
+router.use("/", edit)
 
 export default router;
