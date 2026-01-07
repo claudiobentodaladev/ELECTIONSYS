f7ecdc57ba20a1cc469b41b19936c844f225419d
claudiobentodaladev <programmer.claudio@gmail.com>
2026-01-07 11:09:35 +0100

    endpoint to get own comentary

--- backend/src/routes/candidates/propose/comentary/get.propose_comentary.mjs
+++ backend/src/routes/candidates/propose/comentary/get.propose_comentary.mjs
@@ -32,7 +32,7 @@ router.get("/:candidates_propose_id", (request, response) => {
                             const [{ election_id }] = result;
 
                             mysql.execute(
-                                "SELECT * FROM participation WHERE user_id = ? AND election_id = ?",
+                                "SELECT id FROM participation WHERE user_id = ? AND election_id = ?",
                                 [user.id, election_id], (err, result) => {
                                     if (err) return response.status(500).json(err)
                                     if (result.length === 0) return response.status(404).json({ found: false, message: "candidate not found!" })
@@ -40,6 +40,63 @@ router.get("/:candidates_propose_id", (request, response) => {
                                     mysql.execute(
                                         "SELECT * FROM propose_comentary WHERE candidates_propose_id = ?",
                                         [candidates_propose_id], (err, result) => {
+                                            if (err) return response.status(500).json(err)
+                                            if (result.length === 0) return response.status(404).json({ found: false, message: "candidate not found!" })
+
+                                            return response.status(200).json(result)
+                                        }
+                                    )
+                                }
+                            )
+                        }
+                    )
+                }
+            )
+
+        }
+    )
+
+})
+
+router.get("/my/:candidates_propose_id", (request, response) => {
+    const { user } = request;
+    const { candidates_propose_id } = request.params;
+
+    mysql.execute(
+        "SELECT candidate_id FROM candidates_propose WHERE id = ?",
+        [candidates_propose_id], (err, result) => {
+            if (err) return response.status(500).json(err)
+            if (result.length === 0) return response.status(404).json({ found: false, message: "candidate not found!" })
+
+            const [{ candidate_id }] = result;
+
+            mysql.execute(
+                "SELECT participation_id FROM candidates WHERE id = ?",
+                [candidate_id], (err, result) => {
+                    if (err) return response.status(500).json(err)
+                    if (result.length === 0) return response.status(404).json({ found: false, message: "candidate not found!" })
+
+                    const [{ participation_id }] = result;
+
+                    mysql.execute(
+                        "SELECT election_id FROM participation WHERE id = ?",
+                        [participation_id], (err, result) => {
+                            if (err) return response.status(500).json(err)
+                            if (result.length === 0) return response.status(404).json({ found: false, message: "candidate not found!" })
+
+                            const [{ election_id }] = result;
+
+                            mysql.execute(
+                                "SELECT id FROM participation WHERE user_id = ? AND election_id = ?",
+                                [user.id, election_id], (err, result) => {
+                                    if (err) return response.status(500).json(err)
+                                    if (result.length === 0) return response.status(404).json({ found: false, message: "candidate not found!" })
+
+                                    const [{ id }] = result;
+
+                                    mysql.execute(
+                                        "SELECT * FROM propose_comentary WHERE candidates_propose_id = ? AND participation_id = ?",
+                                        [candidates_propose_id, id], (err, result) => {
                                             if (err) return response.status(500).json(err)
                                             if (result.length === 0) return response.status(404).json({ found: false, message: "candidate not found!" })
 
