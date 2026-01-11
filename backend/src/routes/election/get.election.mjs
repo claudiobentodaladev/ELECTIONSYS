e3a3033af499ae57488e2c9ca7866385d60290c1
claudiobentodaladev <programmer.claudio@gmail.com>
2026-01-11 16:04:59 +0100

    endpoint to get elections for evry user from theme_id and with filter by election_id

--- backend/src/routes/election/get.election.mjs
+++ backend/src/routes/election/get.election.mjs
@@ -1,33 +1,92 @@
 import { Router } from "express";
 import mysql from "../../database/mysql/db.connection.mjs";
+import { found } from "../../utils/response.class.mjs";
 
 const router = Router()
 
-router.get("/my/:theme_id", (request, response) => {
+router.get("/:theme_id", (request, response) => {
     const { user } = request;
     const { theme_id } = request.params;
+    const { election_id } = request.query;
 
-    mysql.execute(
-        "SELECT * FROM theme WHERE user_id = ?",
-        [user.id], (err, result) => {
-            if (err) return response.status(500).json(err)
-            if (result.length === 0) return response.status(200).json({ found: false, message: "election not found!" })
-
-            const [{ id }] = result;
-
-            if (id !== Number(theme_id)) return response.status(404).json({ found: false, message: "election not found!!!" })
-
+    switch (user.role) {
+        case "admin":
             mysql.execute(
-                "SELECT * FROM elections WHERE theme_id = ?",
-                [id], (err, result) => {
-                    if (err) return response.status(500).json(err)
-                    if (result.length === 0) return response.status(404).json({ found: false, message: "There's no election!" })
+                "SELECT id FROM theme WHERE user_id = ? AND id = ?",
+                [user.id, theme_id], (err, result) => {
+                    if (err) return response.status(500).json(new found(err.message).error())
+                    if (result.length === 0) return response.status(200).json({ found: false, message: "election not found!" })
 
-                    return response.status(200).json(result)
+                    const [{ id }] = result;
+
+                    // SAME <-
+                    if (election_id) {
+                        mysql.execute(
+                            "SELECT * FROM elections WHERE id = ? AND theme_id = ?",
+                            [election_id, id], (err, result) => {
+                                if (err) return response.status(500).json(err)
+                                if (result.length === 0) return response.status(404).json({ found: false, message: "There's no election!" })
+
+                                const [Result] = result
+
+                                return response.status(200).json(Result)
+                            }
+                        )
+                    } else {
+                        mysql.execute(
+                            "SELECT * FROM elections WHERE theme_id = ?",
+                            [id], (err, result) => {
+                                if (err) return response.status(500).json(err)
+                                if (result.length === 0) return response.status(404).json({ found: false, message: "There's no election!" })
+
+                                return response.status(200).json(result)
+                            }
+                        )
+                    }
                 }
             )
-        }
-    )
+            break;
+
+        case "eleitor":
+            mysql.execute(
+                "SELECT id FROM theme WHERE id = ?",
+                [theme_id], (err, result) => {
+                    if (err) return response.status(500).json(err)
+                    if (result.length === 0) return response.status(200).json({ found: false, message: "election not found!" })
+
+                    const [{ id }] = result;
+
+                    // SAME <-
+                    if (election_id) {
+                        mysql.execute(
+                            "SELECT * FROM elections WHERE id = ? AND theme_id = ?",
+                            [election_id, id], (err, result) => {
+                                if (err) return response.status(500).json(err)
+                                if (result.length === 0) return response.status(404).json({ found: false, message: "There's no election!" })
+
+                                const [Result] = result
+
+                                return response.status(200).json(Result)
+                            }
+                        )
+                    } else {
+                        mysql.execute(
+                            "SELECT * FROM elections WHERE theme_id = ?",
+                            [id], (err, result) => {
+                                if (err) return response.status(500).json(err)
+                                if (result.length === 0) return response.status(404).json({ found: false, message: "There's no election!" })
+
+                                return response.status(200).json(result)
+                            }
+                        )
+                    }
+                }
+            )
+            break;
+
+        default:
+            break;
+    }
 
 })
 
