61cd57d3461a4f0e7951ebe353fc80e8dce24358
claudiobentodaladev <programmer.claudio@gmail.com>
2026-01-20 19:36:30 +0100

    created created the profile schema valididator

---
+++ backend/src/validator/profile.schema.mjs
@@ -0,0 +1,22 @@
+import { body, checkSchema } from "express-validator";
+
+export const profileSchema = checkSchema({
+  name: {
+    in: ["body"],
+    optional: true,
+    isString: { errorMessage: "name must be a string" },
+    trim: true,
+    notEmpty: { errorMessage: "name cannot be empty" }
+  },
+  surname: {
+    in: ["body"],
+    optional: true,
+    isString: { errorMessage: "surname must be a string" },
+    trim: true
+  },
+  photo_url: {
+    in: ["body"],
+    optional: true,
+    isURL: { errorMessage: "photo_url must be a valid URL" }
+  }
+});
--- backend/src/validator/validator.mjs
+++ backend/src/validator/validator.mjs
@@ -1,26 +1,5 @@
 import { body, checkSchema } from "express-validator";
 
-export const editProfileSchema = checkSchema({
-  name: {
-    in: ["body"],
-    optional: true,
-    isString: { errorMessage: "name must be a string" },
-    trim: true,
-    notEmpty: { errorMessage: "name cannot be empty" }
-  },
-  surname: {
-    in: ["body"],
-    optional: true,
-    isString: { errorMessage: "surname must be a string" },
-    trim: true
-  },
-  photo_url: {
-    in: ["body"],
-    optional: true,
-    isURL: { errorMessage: "photo_url must be a valid URL" }
-  }
-});
-
 export const createThemeSchema = checkSchema({
   title: {
     in: ["body"],
