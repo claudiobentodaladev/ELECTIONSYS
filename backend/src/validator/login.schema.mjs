37a81a528fbac8df1d11377843ac0e895b6053c7
claudiobentodaladev <programmer.claudio@gmail.com>
2026-01-20 19:18:48 +0100

    created the first separated validation schema

---
+++ backend/src/validator/login.schema.mjs
@@ -0,0 +1,21 @@
+import { body, checkSchema } from "express-validator";
+
+export const loginSchema = checkSchema({
+
+  "email": {
+    in: ["body"],
+    isEmail: {
+      errorMessage: "Invalid Email"
+    },
+    normalizeEmail: true
+  },
+
+  "password": {
+    in: ["body"],
+    isLength: {
+      options: { min: 5 },
+      errorMessage: "Password must be length with 5 character"
+    }
+  }
+
+});
--- backend/src/validator/validator.mjs
+++ backend/src/validator/validator.mjs
@@ -1,25 +1,5 @@
 import { body, checkSchema } from "express-validator";
 
-export const loginSchema = checkSchema({
-
-  "email": {
-    in: ["body"],
-    isEmail: {
-      errorMessage: "Invalid Email"
-    },
-    normalizeEmail: true
-  },
-
-  "password": {
-    in: ["body"],
-    isLength: {
-      options: { min: 5 },
-      errorMessage: "Password must be length with 5 character"
-    }
-  }
-
-});
-
 export const signSchema = checkSchema({
   username: {
     in: ["body"],
