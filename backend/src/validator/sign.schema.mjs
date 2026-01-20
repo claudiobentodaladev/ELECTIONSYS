b7c0792ca44e4be1c9c4c2011c207f2f81e96eb3
claudiobentodaladev <programmer.claudio@gmail.com>
2026-01-20 19:21:12 +0100

    the sign validation schema

---
+++ backend/src/validator/sign.schema.mjs
@@ -0,0 +1,65 @@
+import { body, checkSchema } from "express-validator";
+
+export const signSchema = checkSchema({
+  username: {
+    in: ["body"],
+    isString: { errorMessage: "username must be string" },
+    notEmpty: { errorMessage: "username is required" }
+  },
+  email: {
+    in: ["body"],
+    isEmail: { errorMessage: "Invalid Email" },
+    notEmpty: { errorMessage: "Email is required" }
+  },
+
+  password: {
+    in: ["body"],
+    isLength: {
+      options: { min: 6 },
+      errorMessage: "Password must be length with 6 character"
+    }
+  },
+
+  role: {
+    in: ["body"],
+    isIn: {
+      options: [["admin", "eleitor"]],
+      errorMessage: "role must be admin or eleitor"
+    }
+  },
+
+  "profile.name": {
+    in: ["body"],
+    notEmpty: { errorMessage: "name is required" }
+  },
+
+  "profile.surname": {
+    in: ["body"],
+    optional: true
+  },
+
+  "profile.sex": {
+    in: ["body"],
+    optional: true,
+    isIn: {
+      options: [["M", "F"]],
+      errorMessage: "sex must be M or F"
+    }
+  },
+
+  "profile.born_date": {
+    in: ["body"],
+    optional: true,
+    isISO8601: {
+      errorMessage: "born_date must be YYYY-MM-DD"
+    }
+  },
+
+  "profile.photo_url": {
+    in: ["body"],
+    optional: true,
+    isURL: {
+      errorMessage: "photo_url must be a valid URL"
+    }
+  }
+});
