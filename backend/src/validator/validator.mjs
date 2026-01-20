556cd717a37cc019716a8bb045ad6c33a4fd84c1
claudiobentodaladev <programmer.claudio@gmail.com>
2026-01-20 19:43:18 +0100

    created the validation vote schema

--- backend/src/validator/validator.mjs
+++
@@ -1,8 +0,0 @@
-import { body, checkSchema } from "express-validator";
-
-export const voteSchema = checkSchema({
-  candidate_id: {
-    in: ["body"],
-    isMongoId: { errorMessage: "candidate_id must be a valid MongoDB ObjectId" }
-  }
-});
