1ce379793a4813709637b270ed9c98d1cea3f7b5
claudiobentodaladev <programmer.claudio@gmail.com>
2026-01-21 15:31:46 +0100

    created the status auth endpoint

--- backend/src/routes/auth/auth.route.mjs
+++ backend/src/routes/auth/auth.route.mjs
@@ -1,16 +1,18 @@
 import { Router } from "express";
 import { isAuthenticated } from "../../middleware/isAuthenticated.middleware.mjs";
-import {  validator } from "../../middleware/validator.middleware.mjs";
+import { validator } from "../../middleware/validator.middleware.mjs";
 import { loginSchema } from "../../validator/login.schema.mjs";
 import { signSchema } from "../../validator/sign.schema.mjs";
 import sign from "./sign.auth.mjs";
 import login from "./login.auth.mjs";
 import logout from "./logout.auth.mjs";
+import status from "./status.auth.mjs";
 
 const router = Router()
 
 router.use("/sign", signSchema, validator, sign)
 router.use("/login", loginSchema, validator, login)
 router.use("/logout", isAuthenticated, logout)
+router.use("/status", isAuthenticated, status)
 
 export default router;
---
+++ backend/src/routes/auth/status.auth.mjs
@@ -0,0 +1,27 @@
+import { Router } from "express";
+import mysql from "../../database/mysql/db.connection.mjs";
+import { authResponse } from "../../utils/response.class.mjs";
+
+const router = Router();
+
+router.get("/", (request, response) => {
+    const { user } = request;
+
+    mysql.execute(
+        "SELECT id,email,role FROM users WHERE id = ?",
+        [user.id], (err, result) => {
+            if (err) return response.status(500).json(
+                new authResponse(err.message).error()
+            )
+            if (result.length === 0) return response.status(200)
+
+            const [{ id, role }] = result;
+
+            return response.status(200).json(
+                new authResponse().ok({ id, role })
+            )
+        }
+    )
+})
+
+export default router;
