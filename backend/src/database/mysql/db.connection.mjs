c31fe97010ab12191b125d0830070ff22abc28e8
claudiobentodaladev <programmer.claudio@gmail.com>
2026-01-21 09:20:32 +0100

    added mysql password to .env

--- backend/src/database/mysql/db.connection.mjs
+++ backend/src/database/mysql/db.connection.mjs
@@ -3,7 +3,7 @@ import { createPool as connect } from "mysql2";
 export default connect({
     host: "localhost",
     user: "root",
-    password: "olamundo",
+    password: process.env.MYSQL_PASSWORD || "olamundo",
     database: "electionSys",
     waitForConnections: true
 });
