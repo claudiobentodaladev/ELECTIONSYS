4b396f69bc4c445902de4ed38b78ffdac700a948
claudiobentodaladev <programmer.claudio@gmail.com>
2026-01-22 13:47:51 +0100

    organized

--- backend/src/routes/theme/get.theme.mjs
+++ backend/src/routes/theme/get.theme.mjs
@@ -14,7 +14,9 @@ router.get("/", isAdmin, (request, response) => {
             if (err) return response.status(500).json(new found(err.message).error())
             if (result.length === 0) return response.status(200).json(new found("There'no theme created by this user").not())
 
-            return response.status(200).json(new found(null, result).ok("all theme"))
+            return response.status(200).json(
+                new found(null, result).ok("all theme")
+            )
         }
     )
 });
