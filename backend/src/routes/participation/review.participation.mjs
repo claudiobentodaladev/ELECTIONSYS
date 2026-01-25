d070e77fd357cd3003991a12b3a932aa9b398152
claudiobentodaladev <programmer.claudio@gmail.com>
2026-01-25 16:14:14 +0100

    used api response in review partcipations

--- backend/src/routes/participation/review.participation.mjs
+++ backend/src/routes/participation/review.participation.mjs
@@ -1,30 +1,30 @@
 import { Router } from "express";
 import mysql from "../../database/mysql/db.connection.mjs";
 import { isAdmin } from "../../middleware/role.middleware.mjs";
-import { review } from "../../utils/response.class.mjs";
+import { apiResponse } from "../../utils/response.class.mjs";
 
 const router = Router();
 
-router.patch("/:election_id/:participation_id", isAdmin, (request, response) => {
-    const { election_id, participation_id } = request.params;
+router.patch("/:participation_id/election/:election_id", isAdmin, (request, response) => {
+    const { participation_id, election_id } = request.params;
     const { user } = request;
     const { status_participation } = request.body;
 
+    // after put in custom validator ==========================================================================
     if (!status_participation) return response.status(400).json(
-        new review("status_participation must be filled").not()
+        new apiResponse("status_participation must be filled").error(true)
     )
-
     if (!["eligible", "ineligible", "blocked"].includes(status_participation)) return response.status(400).json(
-        new review("status_participation must be eligible or ineligible or blocked or voted").not()
+        new apiResponse("status_participation must be eligible or ineligible or blocked or voted").error(true)
     )
-
-    const notReview_response = new review().not("participate")
+    // =============================  after put in custom validator ============================================
+    const notReview_response = new apiResponse("not review the participation").error(true)
 
     mysql.execute(
         "SELECT * FROM theme WHERE user_id = ?",
         [user.id], (err, result) => {
             if (err) return response.status(500).json(
-                new review(err.message).error()
+                new apiResponse(err.message).error(err)
             )
             if (result.length === 0) return response.status(200).json(notReview_response)
 
@@ -32,29 +32,29 @@ router.patch("/:election_id/:participation_id", isAdmin, (request, response) => 
 
             mysql.execute("select * from elections where id = ? and theme_id = ?", [election_id, id], (err, result) => {
                 if (err) return response.status(500).json(
-                    new review(err.message).error()
+                    new apiResponse(err.message).error(err)
                 )
                 if (result.length === 0) return response.status(500).json(notReview_response)
 
                 mysql.execute("SELECT status FROM participation WHERE id = ?", [participation_id], (err, result) => {
                     if (err) return response.status(500).json(
-                        new review(err.message).error()
+                        new apiResponse(err.message).error(err)
                     )
                     if (result.length === 0) return response.status(500).json(notReview_response)
 
                     const [{ status }] = result;
 
                     if (status === "voted") return response.status(400).json(
-                        new review("not reviewed the participation, because the participate already voted").not()
+                        new apiResponse("not reviewed the participation, because the participate already voted").error(true)
                     )
 
                     mysql.execute("UPDATE participation SET status = ? WHERE id = ?", [status_participation, participation_id], (err, result) => {
                         if (err) return response.status(500).json(
-                            new review(err.message).error()
+                            new apiResponse(err.message).error(err)
                         )
 
                         return response.status(200).json(
-                            new review().ok("participation")
+                            new apiResponse("review the participation").ok({ participation_id, election_id })
                         )
                     })
                 })
