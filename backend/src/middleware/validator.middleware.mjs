ed0b03573b7a7d1d4d7f816ce2189e26696e79c6
claudiobentodaladev <programmer.claudio@gmail.com>
2026-01-25 14:59:24 +0100

    used api response in validator

--- backend/src/middleware/validator.middleware.mjs
+++ backend/src/middleware/validator.middleware.mjs
@@ -1,11 +1,11 @@
 import { validationResult } from "express-validator";
-import { validationResponse } from "../utils/response.class.mjs";
+import { apiResponse } from "../utils/response.class.mjs";
 
 export const validator = (request, response, next) => {
     const errors = validationResult(request);
 
     if (!errors.isEmpty()) return response.status(400).json(
-        new validationResponse("validation error").error(errors.array())
+        new apiResponse("validation error").error(errors.array())
     );
     next();
 };
