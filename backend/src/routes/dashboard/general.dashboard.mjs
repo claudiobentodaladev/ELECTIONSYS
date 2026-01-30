c019e61e7633d1c529e5d38eb5a00d7ba9f06dc6
claudiobentodaladev <programmer.claudio@gmail.com>
2026-01-30 15:44:37 +0100

    used api response in general dashboard

--- backend/src/routes/dashboard/general.dashboard.mjs
+++ backend/src/routes/dashboard/general.dashboard.mjs
@@ -1,8 +1,7 @@
 import { Router } from "express";
-import { found } from "../../utils/response.class.mjs";
 import mysql from "../../database/mysql/db.connection.mjs";
+import { apiResponse } from "../../utils/response.class.mjs";
 import { joinedArray } from "../../utils/functions.mjs";
-import { getElectionsByTheme } from "../../utils/sql/sql.helpers.mjs";
 
 const router = Router()
 
@@ -18,7 +17,9 @@ router.get("/", (request, response) => {
     mysql.execute(
         "SELECT id FROM theme WHERE user_id = ?",
         [user.id], (err, result) => {
-            if (err) return response.status(500).json(new found(err.message).error())
+            if (err) return response.status(500).json(
+                new apiResponse(err.message).error(err)
+            )
             if (result.length === 0) {
 
                 status.theme = result.length;
@@ -26,7 +27,7 @@ router.get("/", (request, response) => {
                 status.participation = 0;
 
                 return response.status(200).json(
-                    new found("all admin status", status).ok("status admin")
+                    new apiResponse("all admin status").ok(status)
                 )
             }
 
@@ -37,14 +38,16 @@ router.get("/", (request, response) => {
             mysql.execute(
                 "SELECT id FROM elections WHERE theme_id IN (?)",
                 [themeIDs], (err, result) => {
-                    if (err) return response.status(500).json(new found(err.message).error())
+                    if (err) return response.status(500).json(
+                        new apiResponse(err.message).error(err)
+                    )
                     if (result.length === 0) {
 
                         status.election = result.length;
                         status.participation = 0;
 
                         return response.status(200).json(
-                            new found("all admin status", status).ok("status admin")
+                            new apiResponse("all admin status").ok(status)
                         )
                     }
 
@@ -55,12 +58,14 @@ router.get("/", (request, response) => {
                     mysql.execute(
                         "SELECT id FROM participation WHERE election_id IN (?)",
                         [electionIDs], (err, result) => {
-                            if (err) return response.status(500).json(new found(err.message).error())
+                            if (err) return response.status(500).json(
+                                new apiResponse(err.message).error(err)
+                            )
 
                             status.participation = result.length;
 
                             return response.status(200).json(
-                                new found("all admin status", status).ok("status admin")
+                                new apiResponse("all admin status").ok(status)
                             )
                         }
                     );
