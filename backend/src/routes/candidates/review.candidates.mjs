06d65426a5d8863634889f33fad2791d396f54fd
claudiobentodaladev <programmer.claudio@gmail.com>
2026-01-30 14:11:09 +0100

    used api response in review candidate endpoint

--- backend/src/routes/candidates/review.candidates.mjs
+++ backend/src/routes/candidates/review.candidates.mjs
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import mysql from "../../database/mysql/db.connection.mjs";
 import { isAdmin } from "../../middleware/role.middleware.mjs";
-import { review } from "../../utils/response.class.mjs";
+import { apiResponse } from "../../utils/response.class.mjs";
 
 const router = Router()
 
@@ -11,19 +11,21 @@ router.patch("/:candidate_id", isAdmin, (request, response) => {
     const { status_candidate } = request.body;
 
     if (!status_candidate) return response.status(400).json(
-        new review("status_candidate must be filled").not()
+        new apiResponse("status_candidate must be filled").error(true)
     )
 
-    if (!["eligible","ineligible","blocked"].includes(status_candidate)) return response.status(400).json(
-        new review("status_candidate must be eligible, ineligible and blocked").not()
+    if (!["eligible", "ineligible", "blocked"].includes(status_candidate)) return response.status(400).json(
+        new apiResponse("status_candidate must be eligible, ineligible and blocked").error(true)
     )
 
-    const notReviewedResponse = new review("There's no candidate to review").not()
+    const notReviewedResponse = new apiResponse("There's no candidate to review").error(true)
 
     mysql.execute(
         "SELECT participation_id FROM candidates WHERE id = ?",
         [candidate_id], (err, result) => {
-            if (err) return response.status(500).json(new review(err.message).error())
+            if (err) return response.status(500).json(
+                new apiResponse(err.message).error(err)
+            )
             if (result.length === 0) return response.status(404).json(notReviewedResponse)
 
             const [{ participation_id }] = result;
@@ -31,7 +33,9 @@ router.patch("/:candidate_id", isAdmin, (request, response) => {
             mysql.execute(
                 "SELECT election_id FROM participation WHERE id = ?",
                 [participation_id], (err, result) => {
-                    if (err) return response.status(500).json(new review(err.message).error())
+                    if (err) return response.status(500).json(
+                        new apiResponse(err.message).error(err)
+                    )
                     if (result.length === 0) return response.status(404).json(notReviewedResponse)
 
                     const [{ election_id }] = result;
@@ -39,7 +43,9 @@ router.patch("/:candidate_id", isAdmin, (request, response) => {
                     mysql.execute(
                         "SELECT * FROM elections WHERE id = ?",
                         [election_id], (err, result) => {
-                            if (err) return response.status(500).json(new review(err.message).error())
+                            if (err) return response.status(500).json(
+                                new apiResponse(err.message).error(err)
+                            )
                             if (result.length === 0) return response.status(404).json(notReviewedResponse)
 
                             const [{ theme_id }] = result;
@@ -47,21 +53,27 @@ router.patch("/:candidate_id", isAdmin, (request, response) => {
                             mysql.execute(
                                 "SELECT * FROM theme WHERE id = ? AND user_id = ?",
                                 [theme_id, user.id], (err, result) => {
-                                    if (err) return response.status(500).json(new review(err.message).error())
+                                    if (err) return response.status(500).json(
+                                        new apiResponse(err.message).error(err)
+                                    )
                                     if (result.length === 0) return response.status(404).json(notReviewedResponse)
 
                                     mysql.execute(
                                         "SELECT * FROM candidates WHERE id = ?",
                                         [candidate_id], (err, result) => {
-                                            if (err) return response.status(500).json(new review(err.message).error())
+                                            if (err) return response.status(500).json(
+                                                new apiResponse(err.message).error(err)
+                                            )
                                             if (result.length === 0) return response.status(404).json(notReviewedResponse)
 
                                             mysql.execute(
                                                 "UPDATE candidates SET status = ? LIMIT 1",
                                                 [status_candidate], (err, result) => {
-                                                    if (err) return response.status(500).json(new review(err.message).error())
+                                                    if (err) return response.status(500).json(
+                                                        new apiResponse(err.message).error(err)
+                                                    )
 
-                                                    return response.status(200).json(new review().ok("candadidate",status_candidate))
+                                                    return response.status(200).json(new apiResponse(`reviewed the candadidate to ${status_candidate}`).ok({}))
                                                 }
                                             )
                                         }
