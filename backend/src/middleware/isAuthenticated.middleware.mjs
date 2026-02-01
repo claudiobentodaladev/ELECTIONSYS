66219529a5b2a32667f14bd963739a76f2713e06
claudiobentodaladev <programmer.claudio@gmail.com>
2026-02-01 12:46:28 +0100

    deleted comentary

--- backend/src/middleware/isAuthenticated.middleware.mjs
+++ backend/src/middleware/isAuthenticated.middleware.mjs
@@ -1,5 +1,5 @@
 import { apiResponse } from "../utils/response.class.mjs";
-// not real and true
+
 export const isAuthenticated = (request, response, next) => {
     if (!request.user) return response.status(401).json(
         new apiResponse("not authenticated").error()
