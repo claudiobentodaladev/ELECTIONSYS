6960e297f940630fd0645ea372c68556de90fcd9
claudiobentodaladev <programmer.claudio@gmail.com>
2026-02-01 13:00:39 +0100

    new way

--- backend/src/middleware/notAuthenticated.middleware.mjs
+++ backend/src/middleware/notAuthenticated.middleware.mjs
@@ -3,7 +3,7 @@ import { apiResponse } from "../utils/response.class.mjs";
 export const notAuthenticated = (request, response, next) => {
   const { user } = request;
   if (user) return response.status(400).json(
-    new apiResponse("must be logged out to sign up").error(user)
+    new apiResponse("must be logged out to sign up", request).error(user)
   )
   next()
 }
