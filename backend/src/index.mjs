c865f619f562fce31bab472b344cb6c3682da6e8
claudiobentodaladev <programmer.claudio@gmail.com>
2026-02-04 15:43:08 +0100

    Bug fix: json request doen't got

--- backend/src/index.mjs
+++ backend/src/index.mjs
@@ -1,9 +1,11 @@
 import express from "express";
 import routes from "./routes/index.routes.mjs";
-import "./config/general.config.mjs";
+import config from "./config/general.config.mjs";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(config)
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`)
